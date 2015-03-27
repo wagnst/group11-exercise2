cccbe76022bfe57a72119df7258e4951960ffb3f
maxGross <maxgross@t-online.de>
2015-03-27 15:00:41 +0100

    Test für höhere ExchangeRate implementiert

--- src/de/wagnst/tpe/exercises/exercise2/test/TestCurrency.java
+++ src/de/wagnst/tpe/exercises/exercise2/test/TestCurrency.java
@@ -1,6 +1,7 @@
 package de.wagnst.tpe.exercises.exercise2.test;
 
 import static org.junit.Assert.assertEquals;
+import de.wagnst.tpe.exercises.exercise2.master.Currency;
 
 import org.junit.Test;
 import de.wagnst.tpe.exercises.exercise2.master.Currencies;
@@ -12,7 +13,7 @@ public class TestCurrency {
      */
     @Test
     public void TestToString() {
-
+        //regular ExchangeRates
         assertEquals("Dollar [$] 1 $ = 1,0000 $", Currencies.DOLLAR.toString());
         assertEquals("Euro [€] 1 $ = 1,2690 €", Currencies.EURO.toString());
         assertEquals("Yen [¥] 1 $ = 0,0091 ¥", Currencies.YEN.toString());
@@ -20,6 +21,11 @@ public class TestCurrency {
                 Currencies.RUBEL.toString());
         assertEquals("Schweizer Franken [CHF] 1 $ = 1,0509 CHF",
                 Currencies.CHF.toString());
+
+        //rate with more than one digit infront of the decimal point
+        Currency HigherExchangeRate = new Currency("Test", "?!", 1200000);
+        assertEquals("Test [?!] 1 $ = 120,0000 ?!",
+                HigherExchangeRate.toString());
 
     }
 }
