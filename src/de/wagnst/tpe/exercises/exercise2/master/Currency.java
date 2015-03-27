02d965198a9dc51b20f150e2e3dc3e3e4bb1fe5d
maxGross <maxgross@t-online.de>
2015-03-27 15:15:14 +0100

    Variablen umbenannt, Kommentare zum Code vervollständigt

--- src/de/wagnst/tpe/exercises/exercise2/master/Currency.java
+++ src/de/wagnst/tpe/exercises/exercise2/master/Currency.java
@@ -42,49 +42,48 @@ public class Currency {
     }
 
     /**
-     * Wandelt Eingabe Wechselkurs in Funschformat euro,cent um; Ohne Verlust
-     * durch Rundungsfehler
+     * Wandelt Wechselkurs in Funschformat euro,cent um; Ohne Verlust durch
+     * Rundungsfehler
      * 
      * @param exchangeRate
      *            Wechselkurs zum Dollar Ganzzahl
      * @return Wechselkurs codiert als String
      */
     private String convertToString(long exchangeRate) {
-        int size = this.digit;
-        String euro = "";
-        String cent = "";
+        // this.digit gives no information about how many
+        // digits are infront of the deciaml point;
+        // atleast one (0,001)
+        int arraySize = this.digit + 1;
+        String infrontOf = "";
+        String behindOf = "";
 
-        // counting digits infront of the deciaml point; save this Information
-        // at variable size
-        while ((exchangeRate / (int) Math.pow(10, size + 1)) > 0) {
-            size++;
+        // get the array size for all digits; save this at arraySize
+        while ((exchangeRate / (int) Math.pow(10, arraySize)) > 0) {
+            arraySize++;
         }
-
-        // creat array with space for all digits
-        long[] digit = new long[size + 1];
+        long[] digit = new long[arraySize];
 
         // digits get set into the array
-        for (int i = size; i >= 0; i--) {
+        for (int i = arraySize - 1; i >= 0; i--) {
             digit[i] = exchangeRate % 10;
             exchangeRate /= 10;
         }
-        int centPointer = size + 1 - this.digit;
         int euroPointer = 0;
+        int centPointer = arraySize - this.digit;
 
-        // creat String of digits infront of the decimal point
+        // creat String with all digits infront of the decimal point
         while (euroPointer < centPointer) {
-            euro += digit[euroPointer];
+            infrontOf += digit[euroPointer];
             euroPointer++;
         }
 
-        // creat Sting of digits behind the decimal point
-
+        // creat String with all digits behind the decimal point
         while ((centPointer) < digit.length) {
-            cent += digit[centPointer];
+            behindOf += digit[centPointer];
             centPointer++;
         }
 
-        return "" + euro + "," + cent;
+        return "" + infrontOf + "," + behindOf;
     }
 
     /**
