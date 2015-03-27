4284eb58d25528272091cde7db12c1e5c0778536
maxGross <maxgross@t-online.de>
2015-03-27 11:06:22 +0100

    Unnötigen Import gelöscht; Test für ToString Methode implementiert

--- src/de/wagnst/tpe/exercises/exercise2/test/TestCurrency.java
+++ src/de/wagnst/tpe/exercises/exercise2/test/TestCurrency.java
@@ -3,15 +3,23 @@ package de.wagnst.tpe.exercises.exercise2.test;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
-
-import de.wagnst.tpe.exercises.exercise2.master.Currency;
+import de.wagnst.tpe.exercises.exercise2.master.Currencies;
 
 public class TestCurrency {
 
+    /**
+     * Testet ob Rückgabestring im korrekten Format ist
+     */
     @Test
     public void TestToString() {
 
-        Currency currency = new Currency("Rubel", "RUB", 255);
-        assertEquals("Rubel [RUB] 1 $ = 0,0255 RUB", currency.toString());
+        assertEquals("Dollar [$] 1 $ = 1,0000 $", Currencies.DOLLAR.toString());
+        assertEquals("Euro [€] 1 $ = 1,2690 €", Currencies.EURO.toString());
+        assertEquals("Yen [¥] 1 $ = 0,0091 ¥", Currencies.YEN.toString());
+        assertEquals("Rubel [RUB] 1 $ = 0,0255 RUB",
+                Currencies.RUBEL.toString());
+        assertEquals("Schweizer Franken [CHF] 1 $ = 1,0509 CHF",
+                Currencies.CHF.toString());
+
     }
 }
