3e00a3d3502c400f1dec7b37f52a0d6894e0dd13
maxGross <maxgross@t-online.de>
2015-03-31 16:20:27 +0200

    subtitel eingebunden (vorher nicht richtig verstanden)

--- src/de/wagnst/tpe/exercises/exercise2/master/Currencies.java
+++ src/de/wagnst/tpe/exercises/exercise2/master/Currencies.java
@@ -10,17 +10,19 @@ package de.wagnst.tpe.exercises.exercise2.master;
 public final class Currencies {
 
     /** US-Dollar */
-    public static final Currency DOLLAR = new Currency("Dollar", "$", 10000);
+    public static final Currency DOLLAR = new Currency("Dollar", "$", 10000,
+            true);
     /** EURO */
-    public static final Currency EURO = new Currency("Euro", "€", 12690);
+    public static final Currency EURO = new Currency("Euro", "€", 12690, true);
 
     /** Japanischer Yen */
-    public static final Currency YEN = new Currency("Yen", "¥", 91);
+    public static final Currency YEN = new Currency("Yen", "¥", 91, false);
 
     /** Rubel der russischen FÃ¶deration */
-    public static final Currency RUBEL = new Currency("Rubel", "RUB", 255);
+    public static final Currency RUBEL = new Currency("Rubel", "RUB", 255,
+            false);
 
     /** Schweizer Franken */
     public static final Currency CHF = new Currency("Schweizer Franken", "CHF",
-            10509);
+            10509, true);
 }
