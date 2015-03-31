98eeebc72a2c1e364681fb48c68d86f6810c6b43
maxGross <maxgross@t-online.de>
2015-03-31 13:25:13 +0200

    Test für toLong angepasst; prüft ob bei negativer Werteingabe toLong positiven Wert zurück gibt

--- src/de/wagnst/tpe/exercises/exercise2/test/TestAmount.java
+++ src/de/wagnst/tpe/exercises/exercise2/test/TestAmount.java
@@ -8,7 +8,7 @@ public class TestAmount {
 
     @Test
     public void testCreationWithLong() {
-        Amount amount1 = new Amount(100000, Currencies.EURO);
+        Amount amount1 = new Amount(-100000, Currencies.EURO);
         Amount amount2 = new Amount(100, Currencies.EURO);
 
         assertEquals(1000, amount1.toLong());
@@ -18,7 +18,7 @@ public class TestAmount {
 
     @Test
     public void testCreationWithDouble() {
-        Amount amount1 = new Amount(10.00, Currencies.EURO);
+        Amount amount1 = new Amount(-10.00, Currencies.EURO);
         Amount amount2 = new Amount(0.01, Currencies.EURO);
         Amount amount3 = new Amount(12.993, Currencies.EURO);
 
@@ -27,4 +27,16 @@ public class TestAmount {
         assertEquals(1299, amount3.toLong());
     }
 
+    @Test
+    public void testGetSign() {
+        Amount amount1 = new Amount(-10000, Currencies.EURO);
+        Amount amount2 = new Amount(10000, Currencies.EURO);
+        Amount amount3 = new Amount(-10.000, Currencies.EURO);
+        Amount amount4 = new Amount(10.000, Currencies.EURO);
+        
+        assertEquals(-1, amount1.getSign());
+        assertEquals(1,amount2.getSign());
+        assertEquals(-1,amount3.getSign());
+        assertEquals(1,amount4.getSign());
+    }
 }
