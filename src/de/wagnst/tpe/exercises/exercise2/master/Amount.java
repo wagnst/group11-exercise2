b889858eb91e57d00da6c96166519410e3b5923c
maxGross <maxgross@t-online.de>
2015-03-31 13:11:23 +0200

    Konstruktoren + toLong Methode implementiert

--- src/de/wagnst/tpe/exercises/exercise2/master/Amount.java
+++ src/de/wagnst/tpe/exercises/exercise2/master/Amount.java
@@ -1,11 +1,22 @@
 package de.wagnst.tpe.exercises.exercise2.master;
 
-public class Amount {
+public final class Amount {
+
+    private long amount;
+    private Currency currency;
+    private boolean chop;
 
     public Amount(long amount, Currency currency) {
+
+        this.amount = amount;
+        this.currency = currency;
+        this.chop = false;
     }
 
     public Amount(double amount, Currency currency) {
+        // chop amount after second decimal digit
+        this((long) (amount * 100), currency);
+        this.chop = true;
     }
 
     public Amount add(Amount other) {
@@ -23,10 +34,33 @@ public class Amount {
     public Amount percentage(int percent) {
     }
 
+    /**
+     * Gibt Information über die Höhe des gespeicherten Betrages
+     * 
+     * @return long amount, jedoch mit nur zwei Nachkommastellen
+     */
     public long toLong() {
+
+        if (!chop) {
+            // chop 3rd and 4th digit
+            return this.amount / 100;
+        } else {
+            return this.amount;
+        }
     }
 
+    /**
+     * prüft ob der Betrag positiv ist
+     * 
+     * @return +1 = Betrag >= 0 ; -1 = Betrag < 0
+     */
     public int getSign() {
+
+        if (amount >= 0) {
+            return 1;
+        } else {
+            return -1;
+        }
     }
 
     public double toDouble() {
