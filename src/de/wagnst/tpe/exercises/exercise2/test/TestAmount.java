03ca2fb28a02fb13169ac82a176ba45e84d7c373
maxGross <maxgross@t-online.de>
2015-03-31 13:12:10 +0200

    Erzeugung für Long und Double Konstruktor begonnen

---
+++ src/de/wagnst/tpe/exercises/exercise2/test/TestAmount.java
@@ -0,0 +1,30 @@
+package de.wagnst.tpe.exercises.exercise2.test;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import de.wagnst.tpe.exercises.exercise2.master.*;
+
+public class TestAmount {
+
+    @Test
+    public void testCreationWithLong() {
+        Amount amount1 = new Amount(100000, Currencies.EURO);
+        Amount amount2 = new Amount(100, Currencies.EURO);
+
+        assertEquals(1000, amount1.toLong());
+        assertEquals(1, amount2.toLong());
+
+    }
+
+    @Test
+    public void testCreationWithDouble() {
+        Amount amount1 = new Amount(10.00, Currencies.EURO);
+        Amount amount2 = new Amount(0.01, Currencies.EURO);
+        Amount amount3 = new Amount(12.993, Currencies.EURO);
+
+        assertEquals(1000, amount1.toLong());
+        assertEquals(1, amount2.toLong());
+        assertEquals(1299, amount3.toLong());
+    }
+
+}
