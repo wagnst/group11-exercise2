5d06a4c58a601a00d01ab3930c9574ca701b112c
wagnst <mail@steffenwagner.com>
2015-04-13 12:58:33 +0200

    fixed jdoc

--- src/de/wagnst/tpe/exercises/exercise2/master/Currencies.java
+++ src/de/wagnst/tpe/exercises/exercise2/master/Currencies.java
@@ -3,26 +3,35 @@ package de.wagnst.tpe.exercises.exercise2.master;
 /**
  * enthält vorgefertigte Objekte von Währungen mit den jeweiligen Wechselkursen,
  * damit man diese einfach innerhalb des Programms wiederverwenden kann.
- * 
- * @author Max
  *
+ * @author Max
  */
 public final class Currencies {
 
-    /** US-Dollar */
+    /**
+     * US-Dollar
+     */
     public static final Currency DOLLAR = new Currency("Dollar", "$", 10000,
             true);
-    /** EURO */
+    /**
+     * EURO
+     */
     public static final Currency EURO = new Currency("Euro", "€", 12690, true);
 
-    /** Japanischer Yen */
+    /**
+     * Japanischer Yen
+     */
     public static final Currency YEN = new Currency("Yen", "¥", 91, false);
 
-    /** Rubel der russischen Föderation */
+    /**
+     * Rubel der russischen Föderation
+     */
     public static final Currency RUBEL = new Currency("Rubel", "RUB", 255,
             false);
 
-    /** Schweizer Franken */
+    /**
+     * Schweizer Franken
+     */
     public static final Currency CHF = new Currency("Schweizer Franken", "CHF",
             10509, true);
 }
