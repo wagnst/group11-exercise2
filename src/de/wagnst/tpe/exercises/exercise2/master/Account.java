c99c05b5a6a49849fca448f55e322d1c55ee3a94
wagnst <mail@steffenwagner.com>
2015-04-16 12:42:24 +0200

    initial simple implementation and javadoc

--- src/de/wagnst/tpe/exercises/exercise2/master/Account.java
+++ src/de/wagnst/tpe/exercises/exercise2/master/Account.java
@@ -1,30 +1,58 @@
 package de.wagnst.tpe.exercises.exercise2.master;
 
+/**
+ * Account manages bookings of money to an account. Every account has an owner
+ * as well as a currency
+ *
+ * @author wagnst / d059727
+ */
 public class Account {
 
+    private Currency currency;
+    private String owner;
+
+    /**
+     * @param owner    name of the account owner (format [String String]
+     * @param currency currency of the account's money
+     */
     public Account(String owner, Currency currency) {
+        this.owner = owner;
+        this.currency = currency;
     }
 
-    public void post(Amount betrag) {
+    /**
+     * book an amount of money to the account (can be positive or negative) and
+     * also convert to the correct currency
+     *
+     * @param amount amount of money
+     */
+    public void post(Amount amount) {
     }
 
+    /**
+     * Show saldo of account (sum of all transactions)
+     *
+     * @return sum of transactions
+     */
     public Amount total() {
         return null;
     }
 
     public Currency getCurrency() {
-        return null;
+        return this.currency;
     }
 
     public String getOwner() {
-        return null;
+        return this.owner;
     }
 
-    public String statement() {
-        return null;
-    }
-
-    public void accountFee(int promille) {
+    /**
+     * set of fees which are charged to the account (product of saldo and
+     * promills)
+     *
+     * @param promills
+     */
+    public void accountFee(int promills) {
     }
 
     public String toString() {
