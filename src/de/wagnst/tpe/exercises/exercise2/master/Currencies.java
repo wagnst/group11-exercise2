b44c142cffdd79ba1a126e981020a4c59be337f9
maxGross <maxgross@t-online.de>
2015-04-16 20:30:55 +0200

    ..

--- src/de/wagnst/tpe/exercises/exercise2/master/Currencies.java
+++ src/de/wagnst/tpe/exercises/exercise2/master/Currencies.java
@@ -16,27 +16,30 @@ public final class Currencies {
     private static final int EXCHANGE_YEN = 91;
     private static final int EXCHANGE_RUBEL = 255;
     private static final int EXCHANGE_CHF = 10509;
+    
 
     /**
      * US-Dollar
      */
-    public static final Currency DOLLAR = new Currency("Dollar", "$", EXCHANGE_DOLLAR,
-            true);
+    public static final Currency DOLLAR = new Currency("Dollar", "$",
+            EXCHANGE_DOLLAR, true);
     /**
      * EURO
      */
-    public static final Currency EURO = new Currency("Euro", "€", EXCHANGE_EURO, true);
+    public static final Currency EURO = new Currency("Euro", "€",
+            EXCHANGE_EURO, true);
 
     /**
      * Japanischer Yen
      */
-    public static final Currency YEN = new Currency("Yen", "¥", EXCHANGE_YEN, false);
+    public static final Currency YEN = new Currency("Yen", "¥", EXCHANGE_YEN,
+            false);
 
     /**
      * Rubel der russischen Föderation
      */
-    public static final Currency RUBEL = new Currency("Rubel", "RUB", EXCHANGE_RUBEL,
-            false);
+    public static final Currency RUBEL = new Currency("Rubel", "RUB",
+            EXCHANGE_RUBEL, false);
 
     /**
      * Schweizer Franken
