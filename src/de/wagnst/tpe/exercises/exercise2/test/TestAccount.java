00abd8bc71fc0156008dd45799df8e575e4387d6
wagnst <mail@steffenwagner.com>
2015-04-16 12:42:07 +0200

    sample account test method

---
+++ src/de/wagnst/tpe/exercises/exercise2/test/TestAccount.java
@@ -0,0 +1,36 @@
+package de.wagnst.tpe.exercises.exercise2.test;
+
+import org.junit.Test;
+
+public class TestAccount {
+
+    @Test
+    public void testPost() throws Exception {
+
+    }
+
+    @Test
+    public void testTotal() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCurrency() throws Exception {
+
+    }
+
+    @Test
+    public void testGetOwner() throws Exception {
+
+    }
+
+    @Test
+    public void testAccountFee() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+}
